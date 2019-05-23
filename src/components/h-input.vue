6ab535871c658546fe05d2ceb67d9216e1183cc4
GaryHjy <782316959@qq.com>
2019-05-23 17:57:38 +0800

    完成简单版input组件

---
+++ src/components/h-input.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="h-input">
+    <input v-bind="$attrs" :type="type" :value="value" @input="handleInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HInput",
+  componentName: "HInput",
+  props: {
+    value: {
+      type: String,
+      default: ""
+    },
+    type: {
+      type: String,
+      default: "text"
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handleInput(e) {
+      let value = e.target.value;
+      this.$emit("input", value);
+      this.$parent.$emit("validate");
+    }
+  }
+};
+</script>
