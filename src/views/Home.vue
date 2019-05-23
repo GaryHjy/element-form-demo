56ffe5bd89e81d177c77714fdd7d8202a7a9cbe7
GaryHjy <782316959@qq.com>
2019-05-23 17:57:57 +0800

    完成demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h-form ref="kForm" :model="ruleForm" :rules="rules">
+      <h-form-item label="账户" prop="userName">
+        <h-input v-model="ruleForm.userName"></h-input>
+      </h-form-item>
+      <h-form-item label="密码" prop="password">
+        <h-input type="password" v-model="ruleForm.password"></h-input>
+      </h-form-item>
+      <h-form-item>
+        <el-button type="primary" @click="submitForm('kForm')"
+          >立即创建</el-button
+        >
+      </h-form-item>
+    </h-form>
+
+    <el-form
+      :model="ruleForm"
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      style="margin-top:150px"
+    >
+      <el-form-item label="账户" prop="userName">
+        <el-input v-model="ruleForm.userName"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="ruleForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >立即创建</el-button
+        >
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HForm from "@/components/h-form";
+import HFormItem from "@/components/h-form-item";
+import HInput from "@/components/h-input";
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
+    HForm,
+    HFormItem,
+    HInput
+  },
+  data() {
+    return {
+      ruleForm: {
+        userName: "",
+        password: ""
+      },
+      rules: {
+        userName: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   }
 };
 </script>
